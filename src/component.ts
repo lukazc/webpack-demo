2036dee5487af9c59c754b10758dce43ffe6d984
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-07-08 09:25:20 +0200

    Implement code splitting and lazy loading

--- src/component.ts
+++ src/component.ts
@@ -1,5 +1,10 @@
 export default (text = "Hello world"): HTMLElement => {
     const element = document.createElement("div");
     element.innerHTML = text;
+    element.onclick = () => {
+        import("./lazy").then((lazy) => {
+            element.textContent = lazy.default;
+        }).catch((err) => { console.error(err); });
+    }
     return element;
 };
---
+++ src/lazy.ts
@@ -0,0 +1,1 @@
+export default "Lazy loaded text"
