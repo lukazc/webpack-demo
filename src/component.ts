f90cd41310e84453499876574fa3df2481e94319
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-08-30 22:12:27 +0200

    Implement web worker.

--- src/component.ts
+++ src/component.ts
@@ -2,11 +2,32 @@ export default (text = TITLE_TEXT): HTMLElement => {
     console.log("drop_console test");
     // comment test
     const element = document.createElement("div");
-    element.innerHTML = text;
+
+    const worker = new Worker(
+        new URL("./worker.ts", import.meta.url)
+    );
+    const state = { text };
+    let lazyModule: any;
+
+    worker.addEventListener("message", ({ data: { text } }) => {
+        state.text = text;
+        const workerTextElement = document.createElement("div");
+        workerTextElement.textContent = text;
+        element.appendChild(workerTextElement);
+    });
+    element.innerHTML = state.text;
+
     element.onclick = () => {
-        import("./lazy").then((lazy) => {
-            element.textContent = lazy.default;
-        }).catch((err) => { console.error(err); });
+        worker.postMessage({ text: state.text });
+
+        if(!lazyModule) {
+            import("./lazy").then((lazy) => {
+                lazyModule = lazy;
+                const lazyElement = document.createElement("div");
+                lazyElement.textContent = lazy.default;
+                element.appendChild(lazyElement);
+            }).catch((err) => { console.error(err); });
+        }
     }
     return element;
 };
